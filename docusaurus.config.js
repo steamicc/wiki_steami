b1d23a19980efe0673d41845a98a43aa3e40ef56
Sébastien NEDJAR <sebastien@nedjar.com>
2026-03-20 09:13:14 +0100

    fix: migrate deprecated onBrokenMarkdownLinks to markdown.hooks
    Docusaurus 3.9 deprecated the root-level `onBrokenMarkdownLinks` config
    option. Move it to `markdown.hooks.onBrokenMarkdownLinks` to suppress
    the deprecation warning.
    
    Closes #1

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,7 +13,12 @@ const config = {
   projectName: 'wiki-steami',
 
   onBrokenLinks: 'warn',
-  onBrokenMarkdownLinks: 'warn',
+
+  markdown: {
+    hooks: {
+      onBrokenMarkdownLinks: 'warn',
+    },
+  },
 
   i18n: {
     defaultLocale: 'fr',
