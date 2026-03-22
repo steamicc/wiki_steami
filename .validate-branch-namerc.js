c26ce84a8fbcc2dbba2395675c35c3a9ac5b4634
Sébastien NEDJAR <sebastien@nedjar.com>
2026-03-22 19:04:43 +0100

    fix: improve branch name error message with all accepted formats

--- .validate-branch-namerc.js
+++ .validate-branch-namerc.js
@@ -2,7 +2,10 @@ module.exports = {
   pattern:
     '^(main)$|^(chore|feat|fix|docs|ci|refactor)\\/([a-z0-9]+-)*[a-z0-9]+$|^release\\/v([0-9]+)\\.([0-9]+)\\.([0-9]+)$',
   errorMsg:
-    'La branche que tu essaies de pusher ne respecte pas nos conventions.\n' +
-    'Format attendu : chore|feat|fix|docs|ci|refactor/<description-en-kebab-case>\n' +
+    'Le nom de la branche ne respecte pas nos conventions.\n' +
+    'Formats acceptes :\n' +
+    '  - main\n' +
+    '  - chore|feat|fix|docs|ci|refactor/<description-en-kebab-case>\n' +
+    '  - release/vX.Y.Z\n' +
     'Tu peux la renommer avec : git branch -m <nom-actuel> <nouveau-nom>',
 };
